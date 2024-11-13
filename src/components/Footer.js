f3bd7ef969890249b8adb4f20ab54c1415c38d4f
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2024-11-12 19:29:21 -0600

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '1em', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+      <p>© 2023 MyApp. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
