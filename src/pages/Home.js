350793d8714e3ec17d0e03d5fcad2f5a8d447f3b
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2024-11-12 19:31:45 -0600

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,12 @@
-
+// src/pages/home.js
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of your app.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
